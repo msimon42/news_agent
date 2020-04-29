d4fd4f74afaf02e9faff75c6348e910fe946a0b6
msimon42 <msimon@uab.edu>
2020-04-28 22:33:44 -0600

    Add connection method

--- app/poros/article_service.rb
+++ app/poros/article_service.rb
@@ -1,3 +1,12 @@
 class ArticleService
+  private
+    def connection
+      Faraday.new('http://localhost:5000') do |f|
+        f.adapter Faraday.default_adapter
+      end
+    end
 
-end  
+    def post_request(uri, body)
+
+    end  
+end
